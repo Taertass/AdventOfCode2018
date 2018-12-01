1b97932e2146b19d23570ab5cd413de0bd5c1f8c
Bartal Clementsen <bartalclementsen@hotmail.com>
2018-12-01 23:46:21 +0000

    Update

--- Core.Client/Program.cs
+++ Core.Client/Program.cs
@@ -18,45 +18,65 @@ namespace Core.Client
 
             List<TestDataSets> allTestDataSets = solution.GetTestDataSets();
 
+            int? specificSolution = null;
+            int? specificCase = null;
+
             ConsoleColor color = Console.ForegroundColor;
             Console.WriteLine($"Running tests for {solution.GetType().Name}");
+            Console.WriteLine();
 
-            for (int solutionNumber = 0; solutionNumber < allTestDataSets.Count; solutionNumber++)
+            if (specificSolution != null && specificCase != null)
             {
-                TestDataSets testDataSets = allTestDataSets[solutionNumber];
+                RunOne(solution, allTestDataSets, specificSolution.Value, specificCase.Value);
+            }
+            else
+            {
+                RunAll(solution, allTestDataSets);
 
-                for (int caseNumber = 0; caseNumber < testDataSets.Count; caseNumber++)
-                {
-                    TestDataSet testDataSet = testDataSets[caseNumber];
+                Console.ForegroundColor = color;
 
-                    string result = solution.Solve(solutionNumber + 1, testDataSet.Input);
-
-                    if (result == testDataSet.Result)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine($"[Success] Test solution: {solutionNumber + 1} case: {caseNumber + 1}");
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"[Failed] test solution: {solutionNumber + 1} case: {caseNumber + 1}");
-
-                        Console.WriteLine($"-= Expected =-");
-                        Console.WriteLine($"{testDataSet.Result}");
-
-                        Console.WriteLine($"-= Got =-");
-                        Console.WriteLine($"{result}");
-                    }
-                }
+                Console.WriteLine();
+                Console.WriteLine("----------------------- 0 -----------------------");
+                Console.WriteLine();
+                solution.SolveAndPrint();
             }
 
-            Console.ForegroundColor = color;
-
-            Console.WriteLine();
-            Console.WriteLine("-----------------------");
-            solution.SolveAndPrint();
-
             Console.ReadLine();
+        }
+
+        private static void RunOne(DayBase solution, List<TestDataSets> allTestDataSets, int solutionNumber, int caseNumber)
+        {
+            TestDataSet testDataSet = allTestDataSets[solutionNumber - 1][caseNumber - 1];
+
+            string result = solution.Solve(solutionNumber, testDataSet.Input);
+
+            if (result == testDataSet.Result)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[Success] Test solution: {solutionNumber} case: {caseNumber}");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"[Failed] test solution: {solutionNumber} case: {caseNumber}");
+
+                Console.WriteLine($"-= Expected =-");
+                Console.WriteLine($"{testDataSet.Result}");
+
+                Console.WriteLine($"-= Got =-");
+                Console.WriteLine($"{result}");
+            }
+        }
+
+        private static void RunAll(DayBase solution, List<TestDataSets> allTestDataSets)
+        {
+            for (int solutionNumber = 1; solutionNumber <= allTestDataSets.Count; solutionNumber++)
+            {
+                for (int caseNumber = 1; caseNumber <= allTestDataSets[solutionNumber - 1].Count; caseNumber++)
+                {
+                    RunOne(solution, allTestDataSets, solutionNumber, caseNumber);
+                }
+            }
         }
     }
 }
